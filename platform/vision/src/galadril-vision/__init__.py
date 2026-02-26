97893c840b0f3b3be349aaa9771d542a987e9661
RealHinome <acn@gravitalia.com>
2026-02-26 18:18:31 +0100

    Add Vision pipeline to public interface

--- platform/vision/src/galadril-vision/__init__.py
+++ platform/vision/src/galadril-vision/__init__.py
@@ -1,7 +1,9 @@
 """Vision pipeline service using Daft for the Galadril platform."""
 
 from galadril_vision.config import VisionConfig
+from galadril_vision.pipeline.runner import VisionPipeline
 
 __all__ = [
     "VisionConfig",
+    "VisionPipeline",
 ]

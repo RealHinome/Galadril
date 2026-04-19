ea72cf4a3da5dcf5b15e8fde3eef02d544314428
RealHinome <acn@gravitalia.com>
2026-04-19 14:25:30 +0200

    Add new extensions on postgres database
    Added: postgis, pg_stat_statements, pg_wait_sampling, pg_repack, plpython3u

--- database/docker-entrypoint-initdb.d/003-install-extensions.sh
+++ database/docker-entrypoint-initdb.d/003-install-extensions.sh
@@ -1,25 +1,36 @@
 #!/bin/bash
+set -e
 
-create_sql=`mktemp`
+create_sql=$(mktemp)
+create_sql_postgres_only=$(mktemp)
 
-if [ -z "${POSTGRESQL_CONF_DIR:-}" ]; then
-	POSTGRESQL_CONF_DIR=${PGDATA}
-fi
+TARGET_DB="${POSTGRES_DB:-postgres}"
 
 cat <<EOF >${create_sql}
 CREATE EXTENSION IF NOT EXISTS age;
 CREATE EXTENSION IF NOT EXISTS vector;
 CREATE EXTENSION IF NOT EXISTS vectorscale;
+CREATE EXTENSION IF NOT EXISTS postgis;
+CREATE EXTENSION IF NOT EXISTS pg_stat_statements;
+CREATE EXTENSION IF NOT EXISTS pg_wait_sampling;
+CREATE EXTENSION IF NOT EXISTS pg_repack;
+CREATE EXTENSION IF NOT EXISTS plpython3u;
 EOF
 
-if [ -z "${POSTGRESQL_PASSWORD:-}" ]; then
-	POSTGRESQL_PASSWORD=${POSTGRES_PASSWORD:-}
-fi
-export PGPASSWORD="$POSTGRESQL_PASSWORD"
+# pg_cron has a strict requirement: it can ONLY be installed in the database
+# defined by `cron.database_name` in the server config.
+cat <<EOF >${create_sql_postgres_only}
+CREATE EXTENSION IF NOT EXISTS pg_cron;
+EOF
 
-psql -U "${POSTGRES_USER}" postgres -f ${create_sql}
-psql -U "${POSTGRES_USER}" template1 -f ${create_sql}
+export PGPASSWORD="${POSTGRES_PASSWORD:-${POSTGRESQL_PASSWORD:-}}"
 
-if [ "${POSTGRES_DB:-postgres}" != 'postgres' ]; then
-    psql -U "${POSTGRES_USER}" "${POSTGRES_DB}" -f ${create_sql}
+psql -U "${POSTGRES_USER}" -d postgres -f "${create_sql_postgres_only}"
+psql -U "${POSTGRES_USER}" -d "${TARGET_DB}" -f "${create_sql}"
+
+if [ "${TARGET_DB}" != "postgres" ]; then
+    echo "Initializing extensions in default 'postgres' database..."
+    psql -U "${POSTGRES_USER}" -d postgres -f "${create_sql}"
 fi
+
+rm -f "${create_sql}" "${create_sql_postgres_only}"
